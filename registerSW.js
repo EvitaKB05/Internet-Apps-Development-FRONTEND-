82767c0e4492cdd02d13d09d94c432c5d671bc16
EvitaKB05 <cabelloeva60@gmail.com>
2025-11-10 01:37:32 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Internet-Apps-Development-FRONTEND-/sw.js', { scope: '/Internet-Apps-Development-FRONTEND-/' })})}
